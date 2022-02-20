65b48e7a8bce3e6a74ef7287c057f145e50f5339
Matt Masteller <mattmasteller@gmail.com>
2022-02-20 13:45:31 -0800

    setup domains, resolvers and records

--- ethereum/contracts/Domains.sol
+++ ethereum/contracts/Domains.sol
@@ -5,7 +5,40 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+    // A "mapping" data type to store their names
+    mapping(string => address) public domains;
+
+    // A "mapping" data type to store their values
+    mapping(string => string) public records;
+
     constructor() {
         console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
+    }
+
+    // A register function that adds their name to our mapping
+    function register(string calldata name) public {
+        // Check that the name is unregistered
+        require(domains[name] == address(0));
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
+
+    // This will give us the domain owner's address
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        // Check that the owner is the txn sender
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name)
+        public
+        view
+        returns (string memory)
+    {
+        return records[name];
     }
 }
--- ethereum/scripts/run.js
+++ ethereum/scripts/run.js
@@ -1,8 +1,23 @@
 const main = async () => {
+  // The first person is the deployer, the second is a random account
+  const [owner, randomPerson] = await hre.ethers.getSigners()
+
   const contractFactory = await hre.ethers.getContractFactory('Domains')
   const contract = await contractFactory.deploy()
   await contract.deployed()
   console.log('Contract deployed to:', contract.address)
+  console.log('Contract deployed by:', owner.address)
+
+  let txn = await contract.register('doom')
+  await txn.wait()
+
+  const domainOwner = await contract.getAddress('doom')
+  console.log('Owner of domain:', domainOwner)
+
+  // Trying to set a record that doesn't belong to me!
+  txn = await contract
+    .connect(randomPerson)
+    .setRecord('doom', 'Ha! my domain now!')
 }
 
 const runMain = async () => {

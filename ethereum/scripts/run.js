56a20924741047406333c162326aa1f32c0672f2
Matt Masteller <mattmasteller@gmail.com>
2022-02-20 16:05:29 -0800

    sell domains as NFTs

--- ethereum/scripts/run.js
+++ ethereum/scripts/run.js
@@ -3,21 +3,26 @@ const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners()
 
   const contractFactory = await hre.ethers.getContractFactory('Domains')
-  const contract = await contractFactory.deploy()
+  const contract = await contractFactory.deploy('guitar')
   await contract.deployed()
   console.log('Contract deployed to:', contract.address)
   console.log('Contract deployed by:', owner.address)
 
-  let txn = await contract.register('doom')
+  let txn = await contract.register('strat', {
+    value: hre.ethers.utils.parseEther('0.1'),
+  })
   await txn.wait()
 
-  const domainOwner = await contract.getAddress('doom')
-  console.log('Owner of domain:', domainOwner)
+  const domainOwner = await contract.getAddress('strat')
+  console.log('Owner of domain "strat":', domainOwner)
 
   // Trying to set a record that doesn't belong to me!
-  txn = await contract
-    .connect(randomPerson)
-    .setRecord('doom', 'Ha! my domain now!')
+  // txn = await contract
+  //   .connect(randomPerson)
+  //   .setRecord('doom', 'Ha! my domain now!')
+
+  const balance = await hre.ethers.provider.getBalance(contract.address)
+  console.log('Contract balance:', hre.ethers.utils.formatEther(balance))
 }
 
 const runMain = async () => {

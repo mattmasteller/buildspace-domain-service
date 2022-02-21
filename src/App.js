b6f8d90c8cd54309d5ae20bde13ae3d7f28b4356
Matt Masteller <mattmasteller@gmail.com>
2022-02-20 17:27:52 -0800

    initial react app design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './styles/App.css'
+import twitterLogo from './assets/twitter-logo.svg'
 
-function App() {
+// Constants
+const TWITTER_HANDLE = '_buildspace'
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header-container">
+          <header>
+            <div className="left">
+              <p className="title">🎸 Guitar Name Service</p>
+              <p className="subtitle">
+                Your favorite guitar on the blockchain!
+              </p>
+            </div>
+          </header>
+        </div>
+
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
